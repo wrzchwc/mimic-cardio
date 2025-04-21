4d165170314da4d66e9cebb04e047c35c92d2196
Jakub Wierzchowiec <wierzchowieckuba@gmail.com>
2025-04-21 02:41:44 +0200

    read credentials directly during fetching ecgs

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,8 @@
-from os import getenv
-
 from packages.ecg import fetch_ecg
 from packages.open_ai import query_model
 
 def main():
-    username = getenv("PHYSIONET_USERNAME")
-    password = getenv("PHYSIONET_PASSWORD")
     dicom_path = "files/p16/p16846280/s96459668/96459668_0001.dcm"
-    fetch_ecg(username, password, dicom_path)
+    fetch_ecg(dicom_path)
     response = query_model("Tell me a joke!")
     print(response.output_text)
